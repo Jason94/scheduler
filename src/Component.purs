a44e4c05ef414873138060945fef03d01cd7fd0f
Jason Walker <JasonW94@gmail.com>
2019-02-17 20:58:45 -0600

    Styling and buttons

--- src/Component.purs
+++ src/Component.purs
@@ -1,19 +1,60 @@
 module Component where
 
+import App.Data
 import Prelude
 
-import App.Data (Employee)
-import Data.Array.NonEmpty (NonEmptyArray, head)
+import Data.Array.NonEmpty (NonEmptyArray, head, toArray)
 import Data.Maybe (Maybe(..))
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 
-data Query a = ToggleState a
+-- | I get annoyed writing `class_ $ ClassName "..."` over and over again. This small utility saves
+-- | a few characters all over our HTML.
+css :: forall r i. String -> HH.IProp ( class :: String | r ) i
+css = HP.class_ <<< HH.ClassName
+
+----       Types       ----
+data Query a = Select Employee a
 
 type State =
-  { employee :: Employee
+  { selected :: Employee
   }
+
+-- | Select an employee to be inserted into the schedule
+selectEmployee :: Employee -> State -> State
+selectEmployee employee = _ { selected = employee }
+
+---- Employees Display ----
+
+employeesDisplay :: State -> H.ComponentHTML Query
+employeesDisplay state =
+  HH.div_
+    [ HH.h1_ [ HH.text "Employees" ]
+    , HH.div_ $ map roleDisplay (toArray allRoles)
+    ]
+  where
+    employeeButtonClass :: Employee -> State -> String
+    employeeButtonClass employee { selected } =
+      if employee == selected
+      then "selector__button selector__button--selected"
+      else "selector__button"
+
+    employeeButton e@{ name } =
+      HH.button
+        [ css $ employeeButtonClass e state
+        , HE.onClick (HE.input_ $ Select e)
+        ]
+        [ HH.text name ]
+
+    roleDisplay :: Role -> H.ComponentHTML Query
+    roleDisplay role =
+      HH.fieldset_ $
+        [ HH.legend_ [ HH.text $ show role ] ] <>
+        map employeeButton (employeesForRole role)
+
+----   Main Component  ----
 
 component :: forall m. NonEmptyArray Employee -> H.Component HH.HTML Query Unit Void m
 component employees =
@@ -27,18 +68,17 @@ component employees =
 
   initialState :: State
   initialState =
-    { employee: head employees
+    { selected: head employees
     }
 
   render :: State -> H.ComponentHTML Query
   render state =
     HH.div_
-      [ HH.h1_
-          [ HH.text "Hello foo!" ]
+      [ employeesDisplay state
       ]
 
   eval :: Query ~> H.ComponentDSL State Query Void m
   eval = case _ of
-    ToggleState next -> do
-      _ <- H.modify (\state -> initialState)
+    Select employee next -> do
+      _ <- H.modify_ $ selectEmployee employee
       pure next
--- src/Data.purs
+++ src/Data.purs
@@ -5,8 +5,8 @@ import Prelude
 import Data.Array (filter)
 import Data.Array.NonEmpty (NonEmptyArray, elemIndex, singleton, toArray, (:))
 import Data.Foldable (foldl)
-import Data.Map (Map, empty, insert)
-import Data.Maybe (isJust)
+import Data.Map (Map, empty, insert, lookup)
+import Data.Maybe (fromMaybe, isJust)
 import Data.Newtype (class Newtype)
 
 data Role = Manager | Programmer | Analyst
@@ -20,9 +20,6 @@ instance showRole :: Show Role where
 allRoles :: NonEmptyArray Role
 allRoles = Manager : Programmer : singleton Analyst
 
-test :: Map Int Int
-test = insert 0 0 (empty)
-
 newtype EmployeeName = EmployeeName String
 derive instance newtypeEmployeeName :: Newtype EmployeeName _
 
@@ -30,6 +27,19 @@ type Employee =
   { name :: String
   , roles :: NonEmptyArray Role
   }
+
+allEmployees :: NonEmptyArray Employee
+allEmployees =
+  { name: "Amanda", roles: singleton Manager }
+  : { name: "Frank", roles: Analyst : singleton Manager }
+  : { name: "Donna", roles: singleton Analyst }
+  : { name: "Ellen", roles: singleton Analyst }
+  : { name: "Judy", roles: singleton Analyst }
+  : { name: "Travis", roles: Manager : singleton Programmer }
+  : { name: "Doug", roles: singleton Programmer }
+  : { name: "Adam", roles: singleton Programmer }
+  : { name: "Ivan", roles: singleton Programmer }
+  : singleton { name: "Jason", roles: singleton Programmer }
 
 -- | Check if an employee has a given role
 hasRole :: Role -> Employee -> Boolean
@@ -44,6 +54,12 @@ groupByRole employees = foldl buildForRole empty allRoles
 
     buildForRole :: Map Role (Array Employee) -> Role -> Map Role (Array Employee)
     buildForRole m role = insert role (employeesFor role) m
+
+employeesByRole :: Map Role (Array Employee)
+employeesByRole = groupByRole allEmployees
+
+employeesForRole :: Role -> Array Employee
+employeesForRole role = fromMaybe [] $ lookup role employeesByRole
 
 type Team =
   { name :: String
